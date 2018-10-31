e2c7aff077f4690f633c3f259e248124676fd3c7
ggd666 <1258856929@qq.com>
2018-10-31 13:10:56 +0800

    测试

--- src/main/java/com/jk/service/user/UserServiceImpl.java
+++ src/main/java/com/jk/service/user/UserServiceImpl.java
@@ -1,4 +1,8 @@
 package com.jk.service.user;
 
 public class UserServiceImpl implements UserService {
+    @Override
+    public void getUserInfo() {
+        System.out.println("username=admin");
+    }
 }
