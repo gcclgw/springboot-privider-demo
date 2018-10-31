2b8a59668491453be3c8e0bbad7ec4f5ef9b5f93
ggd666 <1258856929@qq.com>
2018-10-31 15:12:09 +0800

    ggd

--- src/main/java/com/jk/service/user/UserService.java
+++ src/main/java/com/jk/service/user/UserService.java
@@ -2,6 +2,6 @@ package com.jk.service.user;
 
 public interface UserService {
 
-void getUserInfo();
+
 
 }
--- src/main/java/com/jk/service/user/UserServiceImpl.java
+++ src/main/java/com/jk/service/user/UserServiceImpl.java
@@ -1,8 +1,5 @@
 package com.jk.service.user;
 
 public class UserServiceImpl implements UserService {
-    @Override
-    public void getUserInfo() {
-        System.out.println("username=admin");
-    }
+
 }
