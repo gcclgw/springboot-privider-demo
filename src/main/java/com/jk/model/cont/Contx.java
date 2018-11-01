9852472a2d644cdfabf2566ef9d3e9f1f41ab892
Your Name <email@example.com>
2018-11-01 23:14:09 +0800

    25656

--- src/main/java/com/jk/model/cont/Contx.java
+++ src/main/java/com/jk/model/cont/Contx.java
@@ -1,4 +1,81 @@
 package com.jk.model.cont;
 
-public class Contx {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Contx implements Serializable {
+
+    private static final long serialVersionUID = 4368851814090799453L;
+    private Integer htid;
+    private String htname;
+    private String htstatus;
+    private Date startdate;
+    private Date endtime;
+    private Integer qid;
+    private Date creatdate;
+    private String htinfo;
+
+    public Integer getHtid() {
+        return htid;
+    }
+
+    public void setHtid(Integer htid) {
+        this.htid = htid;
+    }
+
+    public String getHtname() {
+        return htname;
+    }
+
+    public void setHtname(String htname) {
+        this.htname = htname;
+    }
+
+    public String getHtstatus() {
+        return htstatus;
+    }
+
+    public void setHtstatus(String htstatus) {
+        this.htstatus = htstatus;
+    }
+
+    public Date getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(Date startdate) {
+        this.startdate = startdate;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public Integer getQid() {
+        return qid;
+    }
+
+    public void setQid(Integer qid) {
+        this.qid = qid;
+    }
+
+    public Date getCreatdate() {
+        return creatdate;
+    }
+
+    public void setCreatdate(Date creatdate) {
+        this.creatdate = creatdate;
+    }
+
+    public String getHtinfo() {
+        return htinfo;
+    }
+
+    public void setHtinfo(String htinfo) {
+        this.htinfo = htinfo;
+    }
 }
--- src/main/java/com/jk/service/contractx/ContServiceImpl.java
+++ src/main/java/com/jk/service/contractx/ContServiceImpl.java
@@ -1,4 +1,17 @@
 package com.jk.service.contractx;
 
-public class ContServiceImpl {
+import com.jk.mapper.contractx.ContMapper;
+import com.jk.model.cont.Contx;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class ContServiceImpl implements ContService {
+    @Autowired
+    private ContMapper contMapper;
+    @Override
+    public List<Contx> queryCont() {
+
+        return contMapper.queryCont();
+    }
 }
