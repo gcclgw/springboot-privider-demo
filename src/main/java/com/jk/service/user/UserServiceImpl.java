8e3a96c0196ca9981e03667bb3281f851907cee7
gcclgw <505737211@qq.com>
2018-11-06 10:02:08 +0800

    Merge branch 'master' of https://github.com/gcclgw/springboot-privider-demo
    # Conflicts:
    #	src/main/java/com/jk/mapper/orders/OrdersMapper.java
    #	src/main/java/com/jk/service/orders/OrdersService.java
    #	src/main/java/com/jk/service/orders/OrdersServiceImpl.java
    #	src/main/resources/application.yml
    #	src/main/resources/dubbo-provider.xml

--- src/main/java/com/jk/service/user/UserServiceImpl.java
+++ src/main/java/com/jk/service/user/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.jk.service.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 
 
 public class UserServiceImpl implements UserService {
+
 
 }
